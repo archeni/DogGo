1936f5e069cb490911eead2abeacd97856025a18
Traye <johnson.traye@yahoo.com>
2021-06-24 11:45:13 -0500

    first init

---
+++ DogGo.sql
@@ -0,0 +1,5 @@
+﻿
+                        SELECT Walker.Id, Walker.[Name], Walker.ImageUrl, Walker.NeighborhoodId, Neighborhood.Name as Neighborhood
+                        FROM Walker
+                        Left Join Neighborhood on Neighborhood.Id = Walker.NeighborhoodId
+                    
